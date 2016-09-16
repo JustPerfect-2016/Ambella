6354b628dcdb65a6a2887699d6fe3f2024e12d06
AngaPiliso <anga.piliso@gmail.com>
2016-09-16 16:01:50 +0200

    Added Meta on Branding

--- application/controllers/aboutus.php
+++ application/controllers/aboutus.php
@@ -1,14 +1,20 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
 
 class Aboutus extends CI_Controller {
-	
-	public function index()
-	{
-                $this->load->view('header');
-		$this->load->view('about-us');
-                $this->load->view('footer');
-	}
-  
+
+    public function index() {
+        
+        $data['title'] = "Startseite";
+        $data['description'] = "testing 123";
+
+        $this->load->view('header',$data);
+        $this->load->view('about-us');
+        $this->load->view('footer');
+    }
+
 }
 
 /* End of file welcome.php */
--- application/controllers/branding.php
+++ application/controllers/branding.php
@@ -17,69 +17,106 @@ class Branding extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see http://codeigniter.com/user_guide/general/urls.html
 	 */
-	public function index()
-	{
+	public function index(){
+                
+                $data['title'] = "Branding Solutions - Promote your Brand - Branding Services";
+                $data['description'] = "Give your brand a professional branded corporate image. We brand clothing or gifts. Digital printing, screen printing &amp; more. Request a quote today!";
+	
                 $this->load->view('header');
 		$this->load->view('branding/index');
                 $this->load->view('footer');
 	}
        
-        public function debossing()
-	{
+        public function debossing(){
+                
+                $data['title'] = "Debossing Services | Quality Logo Products  - Get a quote";
+                $data['description'] = "We have branding solutions that creates a professional corporate gifts that represents a company’s brand elegantly. Call us today!";
+	
                 $this->load->view('header');
 		$this->load->view('branding/debossing');
                 $this->load->view('footer');
 	}
         
-        public function digitalprinting()
-	{
+        public function digitalprinting(){
+	
+                $data['title'] = "Digital Printing Solutions, For all Digital Printing Needs | Ambella";
+                $data['description'] = "Get a quote for Professional Digital Printing Services For Any Digital Printing solutions! The technique is used for producing logos in excellent quality";
+        
                 $this->load->view('header');
 		$this->load->view('branding/digitalprinting');
                 $this->load->view('footer');
 	}
         
-        public function digitaltransferprinting()
-	{
+        public function digitaltransferprinting(){
+            
+                $data['title'] = "Digital Transfer Printing Solutions - Corporate Branding Services";
+                $data['description'] = "We produce high quality digital transfer printing solutions for you! Call us today, request a quote and you will never regret!";
+                
                 $this->load->view('header');
 		$this->load->view('branding/digitaltransferprinting');
                 $this->load->view('footer');
 	}
         
-        public function embroidery()
-	{
+        public function embroidery(){
+            
+                $data['title'] = "Embroidery Designs - Embroidery Corporate Stiches";
+                $data['description'] = "Embroidery is an excellent way to brand clothing and will always be a good investment when done professionally. Call us today, request a quote!";
+                
                 $this->load->view('header');
 		$this->load->view('branding/embroidery');
                 $this->load->view('footer');
 	}
         
-        public function laserprinting()
-	{
+        public function laserprinting(){
+            
+                $data['title'] = "Laser Printing | Laser Engraving - Laser Printing Solutions";
+                $data['description'] = "Using laser engraving to brand a promotional item produces a flawless design. We are experts when ti comes to laser printing, call us today. Request a quote";
+                
                 $this->load->view('header');
 		$this->load->view('branding/laserprinting');
                 $this->load->view('footer');
 	}
         
-        public function padprinting()
-	{
+        public function padprinting(){
+                
+                $data['title'] = "Pad Printing Services - Pad Printing Solutions - Ambella";
+                $data['description'] = "Pad printing is excellent to use on a number of items for transferring logos and company details, Call us today to request a quote for pad printing.";
+	
                 $this->load->view('header');
 		$this->load->view('branding/padprinting');
                 $this->load->view('footer');
 	}
         
-        public function sublimation()
-	{
+        public function screenprinting(){
+                
+                $data['title'] = "Screen Printing Solutions In South Africa - Ambella";
+                $data['description'] = "We design, we print, full colour screen printing, high quality and fast turn around! Call us to request a quote today!";
+	
+                $this->load->view('header');
+		$this->load->view('branding/screenprinting');
+                $this->load->view('footer');
+	}
+        
+        public function sublimation(){
+                
+                $data['title'] = "Sublimation Printing Johannesburg - Digital Printing SA";
+                $data['description'] = "Get a quote for sublimation printing solutions today. We are a corporate and promotional gifts store that also offers digital printing solutions.";
+	
                 $this->load->view('header');
 		$this->load->view('branding/sublimation');
                 $this->load->view('footer');
 	}
         
-        public function screenprinting()
-	{
+ 
+        public function whybrandedgifts(){
+                
+                $data['title'] = "Branded gifts, corporate and promotional gifts - Ambella Online Store";
+                $data['description'] = "Why you should make branded gifts the center of your marketing strategy. We are leading suppliers of branded and corporate gifts. Contact us now!";
+	
                 $this->load->view('header');
-		$this->load->view('branding/screenprinting');
+		$this->load->view('branding/whybrandedgifts');
                 $this->load->view('footer');
-	}
-         
+	} 
         
 
 }
